99dbd53dcffbf09022da6523d11a84357335f7e7
FayeMao <fayemao68@gmail.com>
2026-01-10 16:44:28 -0500

    Fixed video not showing

--- shehacks-2026/app/components/ObjectDetection.tsx
+++ shehacks-2026/app/components/ObjectDetection.tsx
@@ -194,7 +194,6 @@ const ObjectDetection = () => {
   const drawBoundingBoxes = (predictions: cocoSsd.DetectedObject[], canvas: HTMLCanvasElement) => {
     const ctx = canvas.getContext('2d');
     if (!ctx) return;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     predictions.forEach(prediction => {
       const [x, y, width, height] = prediction.bbox;
